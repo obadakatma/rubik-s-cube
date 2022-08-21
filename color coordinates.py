9fb7144ea1416b9c0568a070850c2d3b63f4398f
obada katma <obadaktm@gmail.com>
2022-08-21 20:17:39 +0300

    updated position of rectangles and returned color values

--- color coordinates.py
+++ color coordinates.py
@@ -1,5 +1,5 @@
 import time
-
+import numpy as np
 import cv2 as cv
 import win32api
 
@@ -14,9 +14,22 @@ capture = cv.VideoCapture(1)
 while True:
     _, frame = capture.read()
     flip = cv.flip(frame, 1)
+
+    hsv = cv.cvtColor(flip, cv.COLOR_BGR2HSV)
+    blur = cv.GaussianBlur(flip, (5, 5), 0)
+    canny = cv.Canny(blur, threshold1=130, threshold2=200)
+
+    # red : hue / saturation /value of brightness
+    low_red = np.array([155, 100, 100])
+    high_red = np.array([180, 170, 255])
+    red_mask = cv.inRange(hsv, low_red, high_red)
+    kernal = np.ones((3, 3), np.uint8)
+    red_mask = cv.erode(red_mask, kernal)
+    red = cv.bitwise_and(flip, flip, mask=red_mask)
     obj = cp.place()
-    rec = cp.place.rec_place(flip)
-    cv.imshow('Video', flip)
+    rec = cp.place.rec_place(red)
+    cv.imshow("red", red)
+
     state_left = win32api.GetKeyState(0x01)
     a = win32api.GetKeyState(0x01)
 
@@ -25,68 +38,69 @@ while True:
         if a < 0:
             print("clicked 1")
             time.sleep(0.5)
-            color_arr[0] = list(flip[flip.shape[1] // 2 - 150, flip.shape[0] // 2 - 150])
+            color_arr[0] = list(red[red.shape[0] // 2 - 150, red.shape[1] // 2 - 150])
             counter = counter + 1
     elif state_left == a and counter == 2:
 
         if a < 0:
             print("clicked 2")
             time.sleep(0.5)
-            color_arr[1] = list(flip[flip.shape[1] // 2, flip.shape[0] // 2 - 150])
+            color_arr[1] = list(red[red.shape[0] // 2 - 150, red.shape[1] // 2])
             counter = counter + 1
     elif state_left == a and counter == 3:
 
         if a < 0:
             print("clicked 3")
             time.sleep(0.5)
-            color_arr[2] = list(flip[flip.shape[1] // 2 + 150, flip.shape[0] // 2 - 150])
+            color_arr[2] = list(red[red.shape[0] // 2 - 150, red.shape[1] // 2 + 150])
             counter = counter + 1
     elif state_left == a and counter == 4:
 
         if a < 0:
             print("clicked 4")
             time.sleep(0.5)
-            color_arr[3] = list(flip[flip.shape[1] // 2 - 150, flip.shape[0] // 2])
+            color_arr[3] = list(red[red.shape[0] // 2, red.shape[1] // 2 - 150])
             counter = counter + 1
     elif state_left == a and counter == 5:
 
         if a < 0:
             print("clicked 5")
             time.sleep(0.5)
-            color_arr[4] = list(flip[flip.shape[1] // 2, flip.shape[0] // 2])
+            color_arr[4] = list(red[red.shape[0] // 2, red.shape[1] // 2])
             counter = counter + 1
     elif state_left == a and counter == 6:
 
         if a < 0:
             print("clicked 6")
             time.sleep(0.5)
-            color_arr[5] = list(flip[flip.shape[1] // 2 + 150, flip.shape[0] // 2])
+            color_arr[5] = list(red[red.shape[0] // 2, red.shape[1] // 2 + 150])
             counter = counter + 1
     elif state_left == a and counter == 7:
 
         if a < 0:
             print("clicked 7")
             time.sleep(0.5)
-            color_arr[6] = list(flip[flip.shape[1] // 2 - 150, flip.shape[0] // 2 + 150])
+            color_arr[6] = list(red[red.shape[0] // 2 + 150, red.shape[1] // 2 - 150])
             counter = counter + 1
     elif state_left == a and counter == 8:
 
         if a < 0:
             print("clicked 8")
             time.sleep(0.5)
-            color_arr[7] = list(flip[flip.shape[1] // 2, flip.shape[0] // 2 + 150])
+            color_arr[7] = list(red[red.shape[0] // 2 + 150, red.shape[1] // 2])
             counter = counter + 1
     elif state_left == a and counter == 9:
 
         if a < 0:
             print("clicked 9")
             time.sleep(0.5)
-            color_arr[8] = list(flip[flip.shape[1] // 2 + 150, flip.shape[0] // 2 + 150])
+            color_arr[8] = list(red[red.shape[0] // 2 + 150, red.shape[1] // 2 + 150])
             counter = counter + 1
     elif state_left == a and counter == 10:
 
         if a < 0:
-            print(color_arr)
+            for lis in color_arr:
+                print(lis[::-1])
             counter = 1
             break
 
--- cube_place.py
+++ cube_place.py
@@ -30,3 +30,31 @@ class place:
 
         cv.rectangle(frame, (frame.shape[1] // 2 + 100, frame.shape[0] // 2 + 100),
                      (frame.shape[1] // 2 + 200, frame.shape[0] // 2 + 200), (211, 211, 211), 3)
+
+        ####################################################
+        # cv.rectangle(frame, (frame.shape[1] // 2 - 150, frame.shape[0] // 2 - 150),
+        #              (frame.shape[1] // 2 - 50, frame.shape[0] // 2 - 50), (211, 211, 211), 3)
+        #
+        # cv.rectangle(frame, (frame.shape[1] // 2 - 50, frame.shape[0] // 2 - 150),
+        #              (frame.shape[1] // 2 + 50, frame.shape[0] // 2 - 50), (211, 211, 211), 3)
+        #
+        # cv.rectangle(frame, (frame.shape[1] // 2 + 50, frame.shape[0] // 2 - 150),
+        #              (frame.shape[1] // 2 + 150, frame.shape[0] // 2 - 50), (211, 211, 211), 3)
+        #
+        # cv.rectangle(frame, (frame.shape[1] // 2 - 150, frame.shape[0] // 2 - 50),
+        #              (frame.shape[1] // 2 - 50, frame.shape[0] // 2 + 50), (211, 211, 211), 3)
+        #
+        # cv.rectangle(frame, (frame.shape[1] // 2 - 50, frame.shape[0] // 2 - 50),
+        #              (frame.shape[1] // 2 + 50, frame.shape[0] // 2 + 50), (211, 211, 211), 3)
+        #
+        # cv.rectangle(frame, (frame.shape[1] // 2 + 50, frame.shape[0] // 2 - 50),
+        #              (frame.shape[1] // 2 + 150, frame.shape[0] // 2 + 50), (211, 211, 211), 3)
+        #
+        # cv.rectangle(frame, (frame.shape[1] // 2 - 150, frame.shape[0] // 2 + 50),
+        #              (frame.shape[1] // 2 - 50, frame.shape[0] // 2 + 150), (211, 211, 211), 3)
+        #
+        # cv.rectangle(frame, (frame.shape[1] // 2 - 50, frame.shape[0] // 2 + 50),
+        #              (frame.shape[1] // 2 + 50, frame.shape[0] // 2 + 150), (211, 211, 211), 3)
+        #
+        # cv.rectangle(frame, (frame.shape[1] // 2 + 50, frame.shape[0] // 2 + 50),
+        #              (frame.shape[1] // 2 + 150, frame.shape[0] // 2 + 150), (211, 211, 211), 3)
